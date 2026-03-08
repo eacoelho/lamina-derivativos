d98cfbcca7b67dcc9e819f2fa0af8da3707e8eba
eacoelho <eduardo@ecoelho.com.br>
2026-03-08 00:21:47 -0300

    Update payoff.py

--- payoff.py
+++ payoff.py
@@ -3,43 +3,56 @@ import numpy as np
 def calculate_payoff(estrutura, strike1, strike2, strike3, custo):
 
     max_strike = max(strike1, strike2, strike3)
-    prices = np.linspace(max_strike*0.6, max_strike*1.4, 300)
+
+    prices = np.linspace(max_strike * 0.6, max_strike * 1.4, 300)
 
     if estrutura == "Long Put":
-        payoff = np.maximum(strike1 - prices,0) - custo
+
+        payoff = np.maximum(strike1 - prices, 0) - custo
 
     elif estrutura == "Long Call":
-        payoff = np.maximum(prices - strike1,0) - custo
+
+        payoff = np.maximum(prices - strike1, 0) - custo
 
     elif estrutura == "Long Put Spread":
+
         payoff = (
-            np.maximum(strike1 - prices,0)
-            - np.maximum(strike2 - prices,0)
+            np.maximum(strike1 - prices, 0)
+            - np.maximum(strike2 - prices, 0)
             - custo
         )
 
     elif estrutura == "Long Call Spread":
+
         payoff = (
-            np.maximum(prices - strike1,0)
-            - np.maximum(prices - strike2,0)
+            np.maximum(prices - strike1, 0)
+            - np.maximum(prices - strike2, 0)
             - custo
         )
 
     elif estrutura == "Zero Cost Collar":
+
         payoff = (
-            np.maximum(strike1 - prices,0)
-            - np.maximum(prices - strike2,0)
+            np.maximum(strike1 - prices, 0)
+            - np.maximum(prices - strike2, 0)
             - custo
         )
 
     elif estrutura == "Seagull":
+
         payoff = (
-            np.maximum(strike1 - prices,0)
-            - np.maximum(prices - strike2,0)
-            + np.maximum(prices - strike3,0)
+            np.maximum(strike1 - prices, 0)
+            - np.maximum(prices - strike2, 0)
+            + np.maximum(prices - strike3, 0)
             - custo
         )
 
-    breakeven = prices[np.argmin(np.abs(payoff))]
+    else:
+
+        payoff = np.zeros_like(prices)
+
+    # cálculo aproximado de breakeven
+    idx = np.argmin(np.abs(payoff))
+    breakeven = prices[idx]
 
     return prices, payoff, breakeven
