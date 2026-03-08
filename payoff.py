543ba0ae828eca8789df72dd47a0cf85a43a7452
eacoelho <eduardo@ecoelho.com.br>
2026-03-08 00:13:53 -0300

    Update payoff.py

--- payoff.py
+++ payoff.py
@@ -3,19 +3,15 @@ import numpy as np
 def calculate_payoff(estrutura, strike1, strike2, strike3, custo):
 
     max_strike = max(strike1, strike2, strike3)
-
-    prices = np.linspace(max_strike*0.6, max_strike*1.4, 200)
+    prices = np.linspace(max_strike*0.6, max_strike*1.4, 300)
 
     if estrutura == "Long Put":
-
         payoff = np.maximum(strike1 - prices,0) - custo
 
     elif estrutura == "Long Call":
-
         payoff = np.maximum(prices - strike1,0) - custo
 
     elif estrutura == "Long Put Spread":
-
         payoff = (
             np.maximum(strike1 - prices,0)
             - np.maximum(strike2 - prices,0)
@@ -23,7 +19,6 @@ def calculate_payoff(estrutura, strike1, strike2, strike3, custo):
         )
 
     elif estrutura == "Long Call Spread":
-
         payoff = (
             np.maximum(prices - strike1,0)
             - np.maximum(prices - strike2,0)
@@ -31,7 +26,6 @@ def calculate_payoff(estrutura, strike1, strike2, strike3, custo):
         )
 
     elif estrutura == "Zero Cost Collar":
-
         payoff = (
             np.maximum(strike1 - prices,0)
             - np.maximum(prices - strike2,0)
@@ -39,7 +33,6 @@ def calculate_payoff(estrutura, strike1, strike2, strike3, custo):
         )
 
     elif estrutura == "Seagull":
-
         payoff = (
             np.maximum(strike1 - prices,0)
             - np.maximum(prices - strike2,0)
@@ -47,4 +40,6 @@ def calculate_payoff(estrutura, strike1, strike2, strike3, custo):
             - custo
         )
 
-    return prices, payoff
+    breakeven = prices[np.argmin(np.abs(payoff))]
+
+    return prices, payoff, breakeven
